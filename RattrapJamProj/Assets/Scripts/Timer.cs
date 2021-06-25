db8ecef437cb12974845c47aa549a852aec514e1
Paul <paul.putegnat@edu.devinci.fr>
2021-06-25 16:23:58 +0200

    Add Timer

--- RattrapJamProj/Assets/Scripts/Timer.cs
+++ RattrapJamProj/Assets/Scripts/Timer.cs
@@ -6,14 +6,27 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text timerText;
-    private int startTime;
+    private float startTime;
+    private bool stop = false;
 
     void Start() {
-
+        startTime = Time.time;
     }
 
     void Update() {
+        if (stop)
+            return;
+        float t = Time.time - startTime;
 
+        string minutesStr = ((int) t / 60).ToString();
+        string secondsStr = (t % 60).ToString("f0");
+        float seconds = (t % 60);
+
+        timerText.text = minutesStr + ":" + secondsStr;
     }
 
+    public void StopTimer()
+    {
+        stop = true;
+    } 
 }
